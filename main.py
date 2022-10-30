adfb1ce46e7596cc59f0962dfeed883e49e02c93
Antonia <antoni.purwadiharja@gmail.com>
2022-10-30 16:51:25 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random
+
+app = Flask(__name__)
+
+welcome_words = ["lovely", "marvelous", "spectacular",
+                 "splendid", "wonderful", "grand",
+                 "phenomenal", "fabulous", "divine",
+                 "terrific", "glorious", "sensational",
+                 "striking", "awe-inspiring", "astounding",
+                 "bewildering", "stupendous", "tremendous",
+                 "peachy", "brilliant", "bodacious",
+                 "dandy", "fantabulous", "magnificent",
+                 "stellar", "exceptional", "noble",
+                 "thrilling", "elite", "crucial",
+                 "vital", "essential", "exclusive"]
+
+
+@app.route('/')
+def home():
+    welcome = random.choice(welcome_words)
+    return render_template("index.html", welcome=welcome)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
